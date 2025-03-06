92f33d41fd39546aaf8c447afaf30fe8944f5719
Sagar Patil <sagarreddypatil@gmail.com>
2025-03-06 09:19:59 -0500

    better benchmark parameters

--- engine/skeleton/benchmark.cu
+++ engine/skeleton/benchmark.cu
@@ -11,8 +11,8 @@
 
 #include <cuda_runtime.h>
 
-static const size_t WARMUP_RUNS = 3;
-static const size_t MINIMUM_RUNS = 10;
+static const size_t WARMUP_RUNS = 10;
+static const size_t MINIMUM_RUNS = 20;
 static const double MINIMUM_TIME_SECS = 1.0;
 
 inline float median(std::vector<float> &v) {
@@ -102,7 +102,7 @@ class BenchmarkRunner {
             cudaDeviceSynchronize();
         }
 
-        while (elapsed < MINIMUM_TIME_SECS && runtimes.size() < MINIMUM_RUNS) {
+        while (elapsed < MINIMUM_TIME_SECS || runtimes.size() < MINIMUM_RUNS) {
             cudaEventRecord(start);
             test_case.launch_kernel(d_inputs, d_outputs, sizes, reinterpret_cast<void *>(solution));
             cudaEventRecord(stop);
