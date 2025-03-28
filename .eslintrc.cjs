f7bc30a54330b56fe2661043def3f1551eccc82b
harmya <hvbhatt@purdue.edu>
2025-03-28 05:58:34 -0400

    make it a warning, code standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    'indent': ['warn', 2],
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
