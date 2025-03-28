6da22f55cef4d2fef314deff2ffb04dccf37b390
harmya <hvbhatt@purdue.edu>
2025-03-28 00:24:06 -0400

    types

--- src/types/problem.ts
+++ src/types/problem.ts
@@ -87,18 +87,3 @@ export type SubmissionEventData = {
   benchmarkResults?: BenchmarkTestResult[];
 };
 
-const SubmissionStatus = {
-  QUEUING: "QUEUING", 
-  COMPILING: "COMPILING",
-  CHECKING: "CHECKING",
-  BENCHMARKING: "BENCHMARKING",
-  ACCEPTED: "ACCEPTED",
-} as const;
-
-const SubmissionError = {
-  COMPILE_ERROR: "Compile Error", 
-  WRONG_ANSWER: "Wrong Answer",
-  RUNTIME_ERROR: "Runtime Error",
-  TIME_LIMIT_EXCEEDED: "Time Limit Exceeded",
-  ERROR: "Error", 
-} as const;
