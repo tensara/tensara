81d58e9ceff752ab6baca70036595c54bb991de9
Harmya Bhatt <hvbhatt@purdue.edu>
2025-03-29 02:30:52 -0400

    profile page updates (#36)
    * profile revamp
    
    * checkpoint
    
    * console cleanup
    
    * use session cleann
    
    * working calender
    
    * remove log
    
    * imports
    
    * make it a dropdown and get all submissions
    
    * add imports
    
    * eveyrthing works
    
    * added smooth scrolling in global and animation for calendar change
    
    * done

--- src/constants/gpu.ts
+++ src/constants/gpu.ts
@@ -7,3 +7,13 @@ export const GPU_DISPLAY_NAMES: Record<string, string> = {
   "L40S": "NVIDIA L40S",
   "L4": "NVIDIA L4"
 } as const;
+
+export const GPU_DISPLAY_ON_PROFILE = {
+  "T4": "T4",
+  "H100": "H100",
+  "A100-80GB": "A100",
+  "A10G": "A10G",
+  "L40S": "L40S",
+  "L4": "L4",
+  "none": "N/A"
+} as const;
--- src/constants/language.ts
+++ src/constants/language.ts
@@ -4,6 +4,11 @@ export const LANGUAGE_DISPLAY_NAMES: Record<string, string> = {
   "python": "Python (Triton)",
 } as const;
 
+export const LANGUAGE_PROFILE_DISPLAY_NAMES: Record<string, string> = {
+  "cuda": "CUDA",
+  "python": "Triton",
+} as const;
+
 export const IS_DISABLED_LANGUAGE: Record<string, boolean> = {
   "cuda": false,
   "python": true,
