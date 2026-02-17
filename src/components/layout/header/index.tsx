f42fc1be96b0155c76595643d2ff17f2e577cc42
harmya <harmyacs@gmail.com>
2026-02-17 15:11:16 -0800

    First pass, minimal UI for learn with GPU puzzles hardcoded

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -56,6 +56,7 @@ export function Header() {
     { label: "Leaderboards", href: "/leaderboard" },
     { label: "Blog", href: "/blog" },
     { label: "Contests", href: "/contests" },
+    { label: "Learn", href: "/learn"}
   ];
 
   const handleSignIn = () => {
